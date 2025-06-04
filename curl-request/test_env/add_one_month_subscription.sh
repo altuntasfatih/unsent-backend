9c2976cdb28604cc75d59ef5e11fe2c9665096b6
Fatih Altuntas <altuntasfatih42@gmail.com>
2025-06-04 13:37:29 +0300

    feat: update

---
+++ curl-request/test_env/add_one_month_subscription.sh
@@ -0,0 +1,13 @@
+curl -X POST "https://altuntasfatih.app.n8n.cloud/webhook-test/add-subscription" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: 2f71a653-2d5c-4ce1-a231-f71e56c9bb77" \
+  -H "device_id: 1234567890" \
+  -d @- <<EOF
+{
+  "user_id": "another_user",
+  "product": "com.unsentpro.monthly",
+  "platform": "ios",
+  "price": 4.99,
+  "currency": "USD"
+}
+EOF
---
+++ curl-request/test_env/get_subscription.sh
@@ -0,0 +1,5 @@
+curl -X GET "https://altuntasfatih.app.n8n.cloud/webhook-test/get-subscription?user_id=another_user" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: 2f71a653-2d5c-4ce1-a231-f71e56c9bb77" \
+  -H "device_id: 1234567890" \
+
