68c800594d08c2f6fbf321b1a4e93bb0eab23ad4
Fatih Altuntas <altuntasfatih42@gmail.com>
2025-06-04 14:13:57 +0300

    feat: update vercel env

---
+++ curl-request/vercel_env/add_one_year_subscription.sh
@@ -0,0 +1,13 @@
+curl -X POST "https://unsent-backend-lyart.vercel.app/api/add-subscription" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: 2f71a653-2d5c-4ce1-a231-f71e56c9bb77" \
+  -H "device_id: 1234567890" \
+  -d @- <<EOF
+{
+  "user_id": "another_user",
+  "product": "com.unsentpro.yearly",
+  "platform": "ios",
+  "price": 19.99,
+  "currency": "USD"
+}
+EOF
---
+++ curl-request/vercel_env/get_subscription.sh
@@ -0,0 +1,7 @@
+curl -X GET "https://unsent-backend-lyart.vercel.app/api/get-subscription?user_id=another_user" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: 2f71a653-2d5c-4ce1-a231-f71e56c9bb77" \
+  -H "device_id: 1234567890" \
+
+
+
