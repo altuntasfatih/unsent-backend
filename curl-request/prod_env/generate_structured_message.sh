440b129845d7b5f50bc72322672bb73f3a076e1c
Fatih Altuntas <altuntasfatih42@gmail.com>
2025-06-05 15:49:44 +0300

    feat: update urls

--- curl-request/prod_env/generate_structured_message.sh
+++ curl-request/prod_env/generate_structured_message.sh
@@ -1,4 +1,4 @@
-curl -X POST "https://altuntasfatih.app.n8n.cloud/webhook-test/generate-message" \
+curl -X POST "https://altuntasfatih.app.n8n.cloud/webhook/generate-structured-message" \
   -H "Content-Type: application/json" \
   -H "Authorization: 2f71a653-2d5c-4ce1-a231-f71e56c9bb77" \
   -H "device_id: 1234567890" \
@@ -7,7 +7,8 @@ curl -X POST "https://altuntasfatih.app.n8n.cloud/webhook-test/generate-message"
   "recipient" : "Allison",
   "messageType" : "Breakup",
   "additionalNotes" : "fuck you allison",
-  "user_id": "another_user",
+  "user_id": "1554d885-2eb0-4e39-8beb-79659d0682b7",
+  "wordSize": 100,
   "answers" : [
     {
       "question" : "What first attracted you to this person?",
